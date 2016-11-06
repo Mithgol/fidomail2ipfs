c870752a06d4b3f686f94ca55dd0809e68567059
Mithgol the Webmaster <getgit@mithgol.ru>
2016-11-07 00:52:39 +0300

    v0.1.1: put EFGH CSS to IPFS, fix error messages, use cached globals

--- fidomail2ipfs.js
+++ fidomail2ipfs.js
@@ -23,14 +23,16 @@ var defaults = {
 var errors = {
    bootstrap: 'Cannot store Bootstrap in IPFS.',
    message: 'Error putting a Fidonet HTML5 message to IPFS.',
-   notArr: 'Not an Array received while putting a FGHI URL to IPFS.',
-   weirdArr: 'Weird array received while putting a FGHI URL to IPFS.',
-   undefinedHash: 'Undefined hash received while putting a FGHI URL to IPFS.'
+   EFGHCSS: 'Error putting EFGHCSS to IPFS.',
+   notArr: 'Not an Array received while putting content to IPFS.',
+   weirdArr: 'Weird array received while putting content to IPFS.',
+   undefinedHash: 'Undefined hash received while putting content to IPFS.'
 };
 
 // cache:
 var templateHTML = false;
 var bootstrapIPFS = false;
+var EFGHCSSIPFSURL = false;
 
 module.exports = (settings, storageDone) => {
    var options = extend({}, defaults, settings);
@@ -39,24 +41,21 @@ module.exports = (settings, storageDone) => {
    async.waterfall([
       // (cached) load the HTML template from the file system:
       callback => {
-         if( templateHTML ) return callback(null, templateHTML);
+         if( templateHTML ) return callback(null); // cached
 
          fs.readFile(
             path.join(__dirname, 'template.html'),
             { encoding: 'utf8' },
             (err, fileData) => {
                if( err ) return callback(err);
-               templateHTML = fileData;
-               return callback(null, templateHTML);
+               templateHTML = fileData; // put to cache
+               return callback(null);
             }
          );
       },
       // (cached) store in IPFS the customized Bootstrap:
-      (templateHTML, callback) => {
-         if( bootstrapIPFS ) return callback(null, {
-            templateHTML: templateHTML,
-            bootstrapIPFS: bootstrapIPFS
-         });
+      callback => {
+         if( bootstrapIPFS ) return callback(null); // cached
 
          IPFS.util.addFromFs(
             path.join(__dirname, 'bootstrap'),
@@ -74,31 +73,66 @@ module.exports = (settings, storageDone) => {
                );
                bootstrapIPFS = arrBootstrapIPFS[
                   arrBootstrapIPFS.length - 1
-               ].hash;
-               if(!( bootstrapIPFS )) return callback(
-                  new Error(errors.bootstrap)
+               ].hash; // put to cache, but is it fine? check:
+               if(!( bootstrapIPFS )){
+                  bootstrapIPFS = false; // invalidate cache
+                  return callback( new Error(errors.bootstrap) );
+               }
+               return callback(null); // cached fine
+            }
+         );
+      },
+      // (cached) store in IPFS the EFGH CSS:
+      callback => {
+         if( EFGHCSSIPFSURL ) return callback(null); // cached
+
+         fs.readFile(
+            EFGH.pathCSS(),
+            { encoding: 'utf8' },
+            (err, contentEFGHCSS) => {
+               if( err ) return callback(err);
+
+               IPFS.add(
+                  Buffer.from(contentEFGHCSS, 'utf8'),
+                  (err, resultIPFS) => {
+                     if( err ) return callback(err);
+                     if(!( resultIPFS )) return callback(
+                        new Error(errors.EFGHCSS)
+                     );
+                     if(!( Array.isArray(resultIPFS) )) return callback(
+                        new Error(errors.notArr)
+                     );
+                     if( resultIPFS.length !== 1 ) return callback(
+                        new Error(errors.weirdArr)
+                     );
+                     var hashIPFS = resultIPFS[0].hash;
+                     if( typeof hashIPFS === 'undefined' ) return callback(
+                        new Error(errors.undefinedHash)
+                     );
+                     // put to cache:
+                     EFGHCSSIPFSURL = 'https://ipfs.io/ipfs/' + hashIPFS;
+                     return callback(null);
+                  }
                );
-               return callback(null, {
-                  templateHTML: templateHTML,
-                  bootstrapIPFS: bootstrapIPFS
-               });
             }
          );
       },
       // generate HTML message, wrap in EFGH, store in IPFS
-      (wrapped, callback) => {
+      callback => {
          var settingsEFGH = extend({}, options, {
             messageHTML: generateFidoHTML.fromText(options.messageText)
          });
-         var templatedHTML = wrapped.templateHTML.replace(
-            /{{bootstrap}}/g, 'https://ipfs.io/ipfs/' + wrapped.bootstrapIPFS
+         var resultingHTML = templateHTML.replace(
+            /{{bootstrap}}/g, 'https://ipfs.io/ipfs/' + bootstrapIPFS
+         ).replace(
+            /{{EFGHCSSIPFSURL}}/g, EFGHCSSIPFSURL
          ).replace(
             /{{title}}/g, escapeHTML(options.subj || '')
          ).replace(
             /{{body}}/g, EFGH.sync(settingsEFGH)
          );
 
-         IPFS.add(Buffer.from(templatedHTML, 'utf8'), (err, resultIPFS) => {
+         IPFS.add(Buffer.from(resultingHTML, 'utf8'), (err, resultIPFS) => {
             if( err ) return callback(err);
             if(!( resultIPFS )) return callback(new Error( errors.message ));
             if(!( Array.isArray(resultIPFS) )) return callback(new Error(
