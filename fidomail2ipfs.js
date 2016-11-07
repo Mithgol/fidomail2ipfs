b41663d1070670f6d72eccc5e33dbc8482edadbc
Mithgol the Webmaster <getgit@mithgol.ru>
2016-11-07 05:19:30 +0300

    v0.1.3: add http://clrs.cc/ colors, add other message text styles

--- fidomail2ipfs.js
+++ fidomail2ipfs.js
@@ -21,18 +21,52 @@ var defaults = {
 };
 
 var errors = {
-   bootstrap: 'Cannot store Bootstrap in IPFS.',
    message: 'Error putting a Fidonet HTML5 message to IPFS.',
-   EFGHCSS: 'Error putting EFGHCSS to IPFS.',
-   notArr: 'Not an Array received while putting content to IPFS.',
-   weirdArr: 'Weird array received while putting content to IPFS.',
-   undefinedHash: 'Undefined hash received while putting content to IPFS.'
+   notArr: 'Not an Array received putting a Fidonet HTML5 message to IPFS.',
+   weirdArr: 'Weird array received putting a Fidonet HTML5 message to IPFS.',
+   undefinedHash: 'Undefined hash after putting a Fidonet message to IPFS.',
+   notArrDir: 'Not an Array received putting a directory to IPFS.',
+   notFoundDir: 'Directory not found in an Array of content put to IPFS.',
+   undefinedDirHash: 'Undefined hash after putting a directory to IPFS.'
 };
 
 // cache:
 var templateHTML = false;
 var bootstrapIPFS = false;
 var EFGHCSSIPFS = false;
+var ourCSSIPFS = false;
+
+var dirToGlobalIPFS = (IPFS, dirPath, dirName, globalName, cbErr) => {
+   if( global[globalName] ) return cbErr(null); // already cached
+
+   IPFS.util.addFromFs(
+      dirPath,
+      { recursive: true },
+      (err, arrIPFS) => {
+         if( err ) return cbErr(err);
+         if(!( Array.isArray(arrIPFS) )) return cbErr(
+            new Error(`[${dirName}] ${errors.notArrDir}`)
+         );
+         var arrDirIPFS = arrIPFS.filter(
+            nextIPFS => (nextIPFS.path || '').endsWith(dirName)
+         );
+         if( arrDirIPFS.length < 1 ) return cbErr(
+            new Error(`[${dirName}] ${errors.notFoundDir}`)
+         );
+         var hashIPFS = arrDirIPFS[ arrDirIPFS.length - 1 ].hash;
+         // if the hash is fine, put to cache and quit
+         if( hashIPFS ){
+            global[globalName] = hashIPFS;
+            return cbErr(null);
+         }
+         // otherwise invalidate cache
+         global[globalName] = false;
+         return cbErr(
+            new Error(`[${dirName}] ${errors.undefinedDirHash}`)
+         );
+      }
+   );
+};
 
 module.exports = (settings, storageDone) => {
    var options = extend({}, defaults, settings);
@@ -54,62 +88,23 @@ module.exports = (settings, storageDone) => {
          );
       },
       // (cached) store in IPFS the customized Bootstrap:
-      callback => {
-         if( bootstrapIPFS ) return callback(null); // cached
-
-         IPFS.util.addFromFs(
-            path.join(__dirname, 'bootstrap'),
-            { recursive: true },
-            (err, arrIPFS) => {
-               if( err ) return callback(err);
-               if(!( Array.isArray(arrIPFS) )) return callback(
-                  new Error(errors.bootstrap)
-               );
-               var arrBootstrapIPFS = arrIPFS.filter(
-                  nextIPFS => (nextIPFS.path || '').endsWith('bootstrap')
-               );
-               if( arrBootstrapIPFS.length < 1 ) return callback(
-                  new Error(errors.bootstrap)
-               );
-               bootstrapIPFS = arrBootstrapIPFS[
-                  arrBootstrapIPFS.length - 1
-               ].hash; // put to cache, but is it fine? check:
-               if(!( bootstrapIPFS )){
-                  bootstrapIPFS = false; // invalidate cache
-                  return callback( new Error(errors.bootstrap) );
-               }
-               return callback(null); // cached fine
-            }
-         );
-      },
+      callback => dirToGlobalIPFS(
+         IPFS,
+         path.join(__dirname, 'bootstrap'), 'bootstrap',
+         'bootstrapIPFS', callback
+      ),
       // (cached) store in IPFS the EFGH CSS:
-      callback => {
-         if( EFGHCSSIPFS ) return callback(null); // cached
-
-         IPFS.util.addFromFs(
-            path.dirname( EFGH.pathCSS() ),
-            { recursive: true },
-            (err, arrIPFS) => {
-               if( err ) return callback(err);
-               if(!( Array.isArray(arrIPFS) )) return callback(
-                  new Error(errors.EFGHCSS)
-               );
-               var arrEFGHCSS = arrIPFS.filter(
-                  nextIPFS => (nextIPFS.path || '').endsWith('styles')
-               );
-               if( arrEFGHCSS.length < 1 ) return callback(
-                  new Error(errors.EFGHCSS)
-               );
-               EFGHCSSIPFS = arrEFGHCSS[ arrEFGHCSS.length - 1 ].hash;
-               // put to cache, but is it fine? check:
-               if(!( EFGHCSSIPFS )){
-                  EFGHCSSIPFS = false; // invalidate cache
-                  return callback( new Error(errors.EFGHCSS) );
-               }
-               return callback(null); // cached fine
-            }
-         );
-      },
+      callback => dirToGlobalIPFS(
+         IPFS,
+         path.dirname( EFGH.pathCSS() ), 'styles',
+         'EFGHCSSIPFS', callback
+      ),
+      // (cached) store in IPFS our CSS:
+      callback => dirToGlobalIPFS(
+         IPFS,
+         path.join(__dirname, 'styles'), 'styles',
+         'ourCSSIPFS', callback
+      ),
       // generate HTML message, wrap in EFGH, store in IPFS
       callback => {
          var settingsEFGH = extend({}, options, {
@@ -119,6 +114,8 @@ module.exports = (settings, storageDone) => {
             /{{bootstrap}}/g, 'https://ipfs.io/ipfs/' + bootstrapIPFS
          ).replace(
             /{{EFGHCSS}}/g, 'https://ipfs.io/ipfs/' + EFGHCSSIPFS
+         ).replace(
+            /{{FidoMail2IPFSCSS}}/g, 'https://ipfs.io/ipfs/' + ourCSSIPFS
          ).replace(
             /{{title}}/g, escapeHTML(options.subj || '')
          ).replace(
